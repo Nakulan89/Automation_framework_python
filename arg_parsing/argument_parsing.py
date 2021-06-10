1d3f2aefb2e50341c39b491f44c73610e1987eac
Nakulan89 <45421193+Nakulan89@users.noreply.github.com>
2021-06-10 15:36:30 +0530

    Created a json example file

--- arg_parsing/argument_parsing.py
+++ arg_parsing/argument_parsing.py
@@ -3,13 +3,13 @@ import os
 import sys
 
 my_parser = argparse.ArgumentParser(description='List the contents of folder', epilog='Enjoy the program ! :)',
-                                    prefix_chars='/')
+                                    prefix_chars='-')
 
-my_parser.add_argument('Path', metavar='path', type=str, help='the path to list')
-x`
+my_parser.add_argument('-path', default='C:\\', required=False, type=str, help='the path to list')
+
 args = my_parser.parse_args()
 
-input_path = args.Path
+input_path = args.path
 
 if not os.path.isdir(input_path):
     print("The specified path does not exist")
