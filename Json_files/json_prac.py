1a26bc59ab887d8619a0c4bfc016fdac08107f6b
Nakulan89 <45421193+Nakulan89@users.noreply.github.com>
2021-06-10 15:45:00 +0530

    Reading a json data from data_file.json

--- Json_files/json_prac.py
+++ Json_files/json_prac.py
@@ -10,5 +10,9 @@ data = {
 with open('data_file.json', 'w') as write_file:
     json.dump(data, write_file, indent=4)
 
+with open('data_file.json', 'r') as read_file:
+    data = json.load(read_file)
+    print(data)
+
 
 
