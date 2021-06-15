b8b31c613f99ce892ad421d66c45cb674144e5f2
Nakulan89 <45421193+Nakulan89@users.noreply.github.com>
2021-06-15 11:54:51 +0530

    Created instance methods of a class and called by a instance objects

---
+++ Class_oops/creating_class_methods.py
@@ -0,0 +1,23 @@
+class Parrot:
+    # class attribute
+    species = "bird"
+
+    # instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sing(self, song):
+        return "{} sings {}".format(self.name, song)
+
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+
+
+# instantiate the parrot class
+blu = Parrot("Blu", 10)
+
+
+# call our instance methods
+print(blu.sing("Happy"))
+print(blu.dance())
