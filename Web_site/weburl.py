32a121b4fe6b922075fe300cc67af2a8fa775a10
Nakulan89 <45421193+Nakulan89@users.noreply.github.com>
2021-06-17 18:22:28 +0530

    storing data into csv file

--- Web_site/weburl.py
+++ Web_site/weburl.py
@@ -1,8 +1,8 @@
 import argparse
 import configparser
+import csv
 import re
 import subprocess
-
 
 my_parser = argparse.ArgumentParser(description="get the user value to print number of url", prefix_chars='-')
 my_parser.add_argument('value', type=int, help='value is a minimum url present in the txt file. Please enter integer '
@@ -19,11 +19,13 @@ user_url = parser.get("website_url", "LogFile")
 url_pattern = "w{3}.\w+.\w+"
 
 user_req_url = []
+lt = []
 count = 0
 
 with open(user_url, "r") as url:
     for web_url in url:
         required_urls = re.findall(url_pattern, web_url)
+        lt.append(required_urls)
         if required_urls != user_req_url:
             for i in required_urls:
                 count = count + 1
@@ -33,19 +35,10 @@ with open(user_url, "r") as url:
     else:
         print("File contains only {} url".format(count))
 
+with open("C:\\Users\\Aravind\\PycharmProjects\\pythonProject\\Web_site\\ip_results.csv", mode='w') as url_data:
+    field_names = ['SL.NO', 'TEXT URL', 'IP ADDRESS']
+    writer = csv.DictWriter(url_data, fieldnames=field_names)
+    writer.writeheader()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for serial_num in range(1, count+1):
+        writer.writerow({field_names[0]: serial_num, field_names[1]: list(filter(lambda x: x, lt[serial_num+1]))})
